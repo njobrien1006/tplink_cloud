94861abbaa46ec4f68bed39152f1e61cf632096d
njobrien1006 <59174199+njobrien1006@users.noreply.github.com>
2021-10-29 12:47:18 -0500

    Intial Release

--- custom_components/TpLink_Cloud/const.py
+++ custom_components/TpLink_Cloud/const.py
@@ -4,18 +4,10 @@ NAME = "Tp-Link Kasa Cloud"
 DOMAIN = "TpLink_Cloud"
 DOMAIN_DATA = f"{DOMAIN}_data"
 VERSION = "0.0.1"
-ATTRIBUTION = "Data provided byhttps://use1-wap.tplinkcloud.com"
+ATTRIBUTION = "Data provided by https://use1-wap.tplinkcloud.com"
 ISSUE_URL = "https://github.com/njobrien1006/tplink_cloud/issues"
 
-# Icons
-ICON = "mdi:format-quote-close"
-
-# Device classes
-BINARY_SENSOR_DEVICE_CLASS = "connectivity"
-
 # Platforms
-#BINARY_SENSOR = "binary_sensor"
-#SENSOR = "sensor"
 SWITCH = "switch"
 PLATFORMS = [SWITCH]
 
