9a0d74d38fc9e4de1dbb49ba62aa30d678824e2e
njobrien1006 <59174199+njobrien1006@users.noreply.github.com>
2021-10-29 12:49:17 -0500

    Initial Release

--- custom_components/TpLink_Cloud/switch.py
+++ custom_components/TpLink_Cloud/switch.py
@@ -1,40 +1,89 @@
 """Switch platform for TpLink_Cloud."""
+import logging
+
 from homeassistant.components.switch import SwitchEntity
 
-from .const import DEFAULT_NAME, DOMAIN, ICON, SWITCH
-from .entity import TpLink_Cloud
+from .const import DEFAULT_NAME, DOMAIN, SWITCH
+from .entity import TpLink_CloudEntity
+from .tplinkcloud import IntegrationBlueprintApiClient
 
+_LOGGER: logging.Logger = logging.getLogger(__package__)
 
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_devices([IntegrationBlueprintBinarySwitch(coordinator, entry)])
+    client = hass.data[DOMAIN][entry.entry_id]
+    deviceslst = await client.async_rtn_devices()
+    await client.async_get_data()
+    #_LOGGER.debug(f"DevicesLst: {deviceslst}")
+    for mydevice in deviceslst:
+        children = await client.get_children_for_device(mydevice["deviceId"])
+        if children == None:
+            async_add_devices([IntegrationBlueprintBinarySwitch(client, mydevice["deviceId"], mydevice["deviceId"], mydevice["alias"])])
+        else:
+            for child in children:
+                async_add_devices([IntegrationBlueprintBinarySwitch(client, mydevice["deviceId"], child["id"], child["alias"])])
 
-
-class IntegrationBlueprintBinarySwitch(TpLink_Cloud, SwitchEntity):
+class IntegrationBlueprintBinarySwitch(SwitchEntity, TpLink_CloudEntity):
     """TpLink_Cloud switch class."""
+
+    def __init__(self, client, deviceId, child_id, alias):
+        self.client = client
+        self.device = deviceId
+        self.child_id = child_id
+        self.alias = alias
+        self.dev_state = None
+        self.dev_alias = None
+
+        # Tell the API client to call dev_update() when it gets an update
+        self.client.set_callback_for_dev(self.device, self.dev_update)
+
+    def dev_update(self):
+        self.dev_state = self.client.get_state_sts(self.child_id)
+        self.dev_alias = self.client.get_alias(self.child_id)
+        
+        # Tell HA we have an update
+        self.schedule_update_ha_state()
 
     async def async_turn_on(self, **kwargs):  # pylint: disable=unused-argument
         """Turn on the switch."""
-        await self.coordinator.api.async_set_title("bar")
-        await self.coordinator.async_request_refresh()
+        await self.client.set_state_for_device(self.child_id, 1)
 
     async def async_turn_off(self, **kwargs):  # pylint: disable=unused-argument
         """Turn off the switch."""
-        await self.coordinator.api.async_set_title("foo")
-        await self.coordinator.async_request_refresh()
+        await self.client.set_state_for_device(self.child_id, 0)
+
+    # Generic Properties
+    @property
+    def available(self):
+        """Reports available."""
+        if self.dev_state == None:
+            return False
+        else:
+            return True
+
+    @property
+    def unique_id(self):
+        return f"{self.child_id.lower()}"
 
     @property
     def name(self):
-        """Return the name of the switch."""
-        return f"{DEFAULT_NAME}_{SWITCH}"
+        if self.dev_alias == None:
+            return
+        return self.dev_alias
+
+    @property
+    def friendly_name(self):
+        if self.dev_alias == None:
+            return
+        return self.dev_alias
 
     @property
     def icon(self):
         """Return the icon of this switch."""
-        return ICON
+        return "mdi:beach"
 
     @property
     def is_on(self):
-        """Return true if the switch is on."""
-        return self.coordinator.data.get("title", "") == "foo"
+        if self.dev_state == None:
+            return 0
+        return self.dev_state
